afbd1aa5a2a78942a7ab90be3fce22b74080d87d
w <490454041@qq.com>
2018-12-20 14:41:11 +0800

    去掉没用的

--- src/components/wordPiece.vue
+++ src/components/wordPiece.vue
@@ -3,38 +3,42 @@
   <div class="app">
     <div class="globalContent">
         <hkm-table
-              :records="records"
-              ref="table"
-              :head-contextmenu="['freeze_line_left', 'freeze_line_right', 'unfreeze_line', 'hide_line']"
-              :body-contextmenu="['copy', 'freeze_row', 'unfreeze_row']"
-              @select="selectLink">
+          :records="records"
+          ref="table"
+          :head-contextmenu="['freeze_line_left', 'freeze_line_right', 'unfreeze_line', 'hide_line']"
+          :body-contextmenu="['copy', 'freeze_row', 'unfreeze_row']"
+          @select="selectLink">
 
-              <hkm-table-column
-                v-for="(column, i) in columns"
-                :key="i"
-                :label="column.label"
-                :type="column.type"
-                :width="column.width"
-                :fixed="column.fixed"
-                :prop="column.prop"
-                :align="column.align"
-                :sortable="column.sortable"
-                :resizable="column.resizable"
-                :editable="column.editable">
-                <!-- <template slot-scope="{row, column}">
-                  {{renderCell(row, column)}}
-                </template> -->
-              </hkm-table-column>
+          <hkm-table-column
+            v-for="(column, i) in columns"
+            :key="i"
+            :label="column.label"
+            :type="column.type"
+            :width="column.width"
+            :fixed="column.fixed"
+            :prop="column.prop"
+            :align="column.align"
+            :sortable="column.sortable"
+            :resizable="column.resizable"
+            :editable="column.editable">
+            <!-- <template slot-scope="{row, column}">
+              {{renderCell(row, column)}}
+            </template> -->
+          </hkm-table-column>
 
-            </hkm-table>
+        </hkm-table>
     </div>
   </div>
 
 </template>
 
 <script>
+import clusterize from "vue-clusterize"
 export default {
   name:'wordPiece',
+  components:{
+    clusterize
+  },
   created(){
     var obj = {"createUserId":7255022,"createTime":1544774693000,"modifyUserId":7255022,"modifyTime":1544774702000,"accountSet":1,"state":"10A","createUserName":null,"modifyUserName":"肖羚","backup1":null,"backup2":null,"backup3":null,"backup4":null,"backup5":null,"contentId":525100,"oldid":null,"purchaseId":525100,"purchaseOrderCode":"CGG0745181214001","contentBrandId":null,"contractCode":null,"contractType":null,"inventoryCode":"C18092700003","inventoryId":1240102,"inventoryName":"1060","purchaseUnit":"num","starUsingBatch":"1","varietyId":1250100,"varietyName":"1系铝","brandId":1240100,"brandName":"1060","statusId":1240101,"statusName":"H24","thickness":0.2,"width":1000.0,"length":null,"specificationModel":"0.2*1000","mainMeasurementUnit":"KG","number":6500.0,"unitPrice":0.01,"systemPrice":0.0,"amount":65.0,"marketDate":1544745600000,"arrivalDate":null,"thicknessTolerance":"-0.001/+0.1","widthTolerance":null,"lengthTolerance":null,"hardnessTolerance":null,"tensileTolerance":null,"weightTolerance":null,"elongationTolerance":null,"custcode":null,"custname":null,"warehouse":null,"transportMode":null,"remark":null,"basePrice":0.0,"lossCost":0.0,"processCost":0.0,"specChange":0.0,"contractChange":0.0,"priceChange":0.0,"businessStatus":"EXE","closeUserId":null,"closeUserName":null,"closeTime":null,"closeReason":null,"closeRemark":null,"statusPrice":0.0,"thicknessPrice":0.0,"widthPrice":0.0,"lengthPrice":0.0,"workUnitPrice":0.0,"freightUnitPrice":0.0,"useUnitPrice":0.0,"useType":"press","onWayNum":0.0,"arrivalNum":6450.0,"orderDate":1544716800000,"lastarrival":1544716800000,"surplusNum":50.0,"custsortname":"明泰C","pricingMode":"hp","exportOrderdate":null,"exportArrivaldate":null,"exportlastArrival":null,"exportClosedate":null}
     for(let i=0;i<500;i++){
