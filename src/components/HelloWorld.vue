de7222263ad3f58742cc3e7c765cdd139451bd54
w <490454041@qq.com>
2019-02-22 17:59:34 +0800

    摄像头

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,6 +12,8 @@
       <li>
         <router-link to="/wordPiece">wordPiece</router-link>
       </li>
+      <input type="file" value="" accept="image/*" id="file"  v-on:change='onUpload($event)' capture multiple="multiple">
+
     </ul>
   </div>
 </template>
@@ -22,8 +24,33 @@ export default {
   data () {
     return {
       msg: '老衲',
-
+      marriage_list
     }
+  },
+  method:{
+    onUpload(e){
+      let array = []
+        for(let i=0;i<e.target.files.length;i++){
+          let formData = new FormData();
+          formData.append('file',e.target.files[i]);
+          array.push(formData)
+        }
+        let pool = array.map(params =>{
+          return this.uploadImg(params)
+        })
+        Promise.all(pool).then((data)=>{
+          this[arr].push(...data);
+        }).catch((e)=>{
+          this.$vux.toast.text(e.message, 'top')
+        }).finally(()=>{
+          this.$vux.loading.hide()
+        })
+    },
+    uploadImg(formData){
+      return this.$http.post("/mgr/upload", formData).then(({ data }) => {
+            return data
+        })
+    },
   }
 }
 </script>
